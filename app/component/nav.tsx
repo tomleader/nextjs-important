4ccc1ad31fe9e49eb1064ee9668ebc3f0c7cc5f6
yuan20406 <>
2025-08-12 11:44:23 +0800

    1111

--- app/component/nav.tsx
+++ app/component/nav.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 export default function Nav() {
   return (
     <div className="nav">
+      <Link href="./">首页</Link>
       <Link href="./ssr">SSR 页面</Link>
       <Link href="./ssg">SSG 页面</Link>
       <Link href="./isr">ISR 页面</Link>
