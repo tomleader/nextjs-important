64231e1ea26fad2428a76975b0b7335a8088a914
yuan20406 <>
2025-08-12 17:54:47 +0800

    1111

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import Image from 'next/image'
 import Nav from './component/nav'
 
 export default function Page() {
+  const now = new Date().toISOString();
   return <div>
     <Nav></Nav>
     <h1>Next.js App Router 混合渲染 Demo</h1>
+    <p>生成时间: {now}</p>
   </div>
 }
