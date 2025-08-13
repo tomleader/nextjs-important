bda99467b14bc722e7ba101a301ece9a22081396
yuan20406 <>
2025-08-13 15:03:41 +0800

    1111

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">404 — 页面未找到 1111</h1>
+    </div>
+  );
+}
