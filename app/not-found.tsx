40472e6e1fbc15c05c4a019c6e4a9ca17f47c77a
yuan20406 <>
2025-08-13 18:00:00 +0800

    1111

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,10 @@
 
-export default function NotFound() {
+export default async function NotFound() {
+  const now = new Date().toLocaleString();
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">404 — 页面未找到 1111</h1>
+      <p>当前时间：{now}</p>
     </div>
   );
 }
